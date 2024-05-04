3fbdf76630a9c8aaff6641dd3ab5e8bb56998efc
Bielz99 <zorzangabrielzds@gmail.com>
2024-05-04 00:38:51 -0300

    Estudando conceito JS

---
+++ 2_tipos_de_dado/4_strings/script.js
@@ -0,0 +1,14 @@
+console.log( typeof "Gabriel Zorzan");
+console.log( typeof 'Aspas simples');
+console.log( typeof `Teste com shif aspas`);
+
+
+console.log("Primeira linha \n Segunda linha");
+
+console.log("O meu nome é 'Gabriel'");
+
+console.log('O meu nome é "Zorzan"');
+
+console.log(`A multiplicação de 5 por e é ${5 * 3}`);
+
+console.log("O "  + "meu" + " nome" + " é" + " Gabriel");
---
+++ 2_tipos_de_dado/5_booleans/script.js
@@ -0,0 +1,38 @@
+//Boolenas Basicos
+console.log(true);
+console.log(false);
+console.log( 10 > 2);
+console.log( 5 > 200);
+
+
+//Booleans Lógicos 
+console.log(3 >= 3);
+console.log(5 <= 3);
+console.log(5 == 3);
+console.log('Gabriel' != 'Gabriel');
+console.log(3 === '3' );
+console.log(4 === 4 );
+console.log(5 > 3 && 3 == 2 );
+console.log(5 > 3 || 3 == 1);
+console.log(3 == 3 || "Gabriel" == "Gabriel");
+console.log("Gabriel" == "Joao" || false);
+console.log(!(true && true));
+console.log(!(true && true));
+
+//Boolean Operador Ternario
+
+console.log(5 > 2 ? 'É sim' : 'É não');
+console.log(false ? 5 : 4);
+console.log("Gabriel" == "Gabriel" ? "Olá Gabriel" : "Não é gabriel");
+
+
+
+
+
+
+
+
+
+
+
+
